4da9e53522ff12a3432d1185b6548164fbbb253f
Qaisar <qaisarali17qaisarali17@gmail.com>
2026-04-08 12:31:15 +0500

    feat: add card-project-1 directory
    Introduce a new directory structure for card-project-1, expanding the project's component ecosystem with dedicated space for card-related implementations.

---
+++ card-project-1/src/App.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='parent'>
+      <div className='card'>
+         <div className="top">
+          <img src="https://i.pinimg.com/736x/fc/d4/1b/fcd41b74a287a4cfb3705c6ebf77ff8e.jpg" alt="" />
+          <button>Save</button>
+         </div>
+
+         <div className="center">
+          <h1>Card Title</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
